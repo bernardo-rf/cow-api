f3cdd2dca0695f2c8c63c9bb2403f2df8edcd210
Bernardo Figueiredo <bernardo.figueiredo@voidsoftware.com>
2023-02-13 14:28:02 +0000

    Fix genealogy query

--- src/main/java/cow/starter/bovine/models/BovineRepository.java
+++ src/main/java/cow/starter/bovine/models/BovineRepository.java
@@ -30,23 +30,7 @@ public interface BovineRepository extends JpaRepository<Bovine, Long> {
     @Query("SELECT b FROM Bovine b WHERE b.idBovine = :idBovine and b.active = true ORDER BY b.idBovine ASC")
     Bovine getBovine(long idBovine);
 
-
-    @Query( value ="WITH temp(id_bovine,id_bovine_parent1, id_bovine_parent2, active, birth_date, breed, color, height, " +
-            "id_contract, id_field, observation, serial_number, weight, gender, id_user, imagecid) as " +
-            "(SELECT S.id_bovine, S.id_bovine_parent1, S.id_bovine_parent2, S.active, S.birth_date, S.breed, S.color, " +
-            "S.height, S.id_contract, S.id_field, S.observation, S.serial_number, S.weight, S.gender, S.id_user, S.imagecid " +
-            "FROM cow_bovine as S " +
-            "WHERE id_bovine = ?1 " +
-            "UNION ALL " +
-            "SELECT S2.id_bovine, S2.id_bovine_parent1, S2.id_bovine_parent2, S2.active, S2.birth_date, S2.breed, S2.color, " +
-            "S2.height, S2.id_contract, S2.id_field, S2.observation, S2.serial_number, S2.weight, S2.gender, S2.id_user, S2.imagecid " +
-            "FROM cow_bovine as S2 INNER JOIN temp on S2.id_bovine=temp.id_bovine_parent1 and temp.id_bovine is not null " +
-            "UNION ALL " +
-            "SELECT S3.id_bovine, S3.id_bovine_parent1, S3.id_bovine_parent2, S3.active, S3.birth_date, S3.breed, S3.color, " +
-            "S3.height, S3.id_contract, S3.id_field, S3.observation, S3.serial_number, S3.weight, S3.gender, S3.id_user, S3.imagecid " +
-            "FROM cow_bovine as S3 INNER JOIN temp on S3.id_bovine=temp.id_bovine_parent2 and temp.id_bovine is not null) " +
-            "SELECT DISTINCT * FROM temp order by id_bovine",
-            nativeQuery = true)
+    @Query(value = "WITH genealogy_temp (id_bovine, id_bovine_parent1, id_bovine_parent2, active, birth_date, breed, color, height, id_contract, id_field, observation, serial_number, weight, gender, id_user, imagecid) AS (SELECT id_bovine, id_bovine_parent1, id_bovine_parent2, active, birth_date, breed, color, height, id_contract, id_field, observation, serial_number, weight, gender, id_user, imagecid FROM cow_bovine WHERE (id_bovine=?1)) SELECT DISTINCT t.* FROM (SELECT * FROM genealogy_temp UNION ALL SELECT S2.id_bovine, S2.id_bovine_parent1, S2.id_bovine_parent2, S2.active, S2.birth_date, S2.breed, S2.color, S2.height, S2.id_contract, S2.id_field, S2.observation, S2.serial_number, S2.weight, S2.gender, S2.id_user, S2.imagecid FROM cow_bovine S2 INNER JOIN genealogy_temp on S2.id_bovine=genealogy_temp.id_bovine_parent1 UNION ALL SELECT S3.id_bovine, S3.id_bovine_parent1, S3.id_bovine_parent2, S3.active, S3.birth_date, S3.breed, S3.color, S3.height,S3.id_contract, S3.id_field, S3.observation, S3.serial_number, S3.weight, S3.gender, S3.id_user, S3.imagecid FROM cow_bovine S3 INNER JOIN genealogy_temp on S3.id_bovine=genealogy_temp.id_bovine_parent2 UNION ALL SELECT S4.id_bovine, S4.id_bovine_parent1, S4.id_bovine_parent2, S4.active, S4.birth_date, S4.breed, S4.color, S4.height, S4.id_contract, S4.id_field, S4.observation, S4.serial_number, S4.weight, S4.gender, S4.id_user, S4.imagecid FROM cow_bovine S4 INNER JOIN genealogy_temp on S4.id_bovine_parent1=genealogy_temp.id_bovine_parent1 AND S4.id_bovine_parent2=genealogy_temp.id_bovine_parent2) t ORDER BY id_bovine ASC", nativeQuery = true)
     List<Bovine> getGenealogy(long idBovine);
 
 
